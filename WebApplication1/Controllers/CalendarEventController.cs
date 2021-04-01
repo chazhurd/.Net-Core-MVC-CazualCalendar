cbe335cd178232731e4b93ad3958792b6cfa44a4
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2021-04-01 15:12:34 -0700

    Dev
    Still in dev, however drawing near to close.

---
+++ WebApplication1/Controllers/CalendarEventController.cs
@@ -0,0 +1,52 @@
+﻿
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CalendarEventController : Controller
+    {
+      [HttpPost]
+      public ActionResult CreateEvent(Events calendarEvent)
+        {
+            var tokenFile = "C:\\Users\\vanil\\source\\repos\\WebApplication1\\WebApplication1\\Files\\token.json";
+            var tokens = JObject.Parse(System.IO.File.ReadAllText(tokenFile));
+
+            RestClient restClient = new RestClient();
+            RestRequest request = new RestRequest();
+
+            calendarEvent.Start.DateTime = DateTime.Parse(calendarEvent.Start.DateTime).ToString("yyyy-MM-dd'T'HH:mm:ss.fffk");
+            calendarEvent.End.DateTime = DateTime.Parse(calendarEvent.End.DateTime).ToString("yyyy-MM-dd'T'HH:mm:ss.fffk");
+
+            var model = JsonConvert.SerializeObject(calendarEvent, new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+
+            });
+
+            request.AddQueryParameter("key", "AIzaSyBPhlWq_ZZqOj1crvD7cOXOCU9VY6cgnWI");
+            request.AddHeader("Authorization", "Bearer " + tokens["access_token"]);
+            request.AddHeader("Accept", "application/json");
+            request.AddHeader("Content-Type", "application/json");
+            request.AddParameter("application/json", model, ParameterType.RequestBody);
+
+            restClient.BaseUrl = new System.Uri("https://www.googleapis.com/calendar/v3/calendars/primary/events");
+            var response = restClient.Post(request);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return RedirectToAction("Index", "Home", new { status = "success" });
+            }
+
+            return View("Error");
+        }
+    }
+}
---
+++ WebApplication1/Controllers/OAuthController.cs
@@ -0,0 +1,103 @@
+﻿
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System.Web.Mvc;
+
+namespace BasicMVCGoogleCalendar.Controllers
+{
+    public class OAuthController:Controller
+    {
+
+        public void Callback(string code, string error, string state)
+        {
+            if (string.IsNullOrWhiteSpace(error))
+            {
+                this.GetTokens(code);
+            }
+        }
+
+        public ActionResult RefreshToken()
+        {
+            var tokenFile = "C:\\Users\\vanil\\source\\repos\\WebApplication1\\WebApplication1\\Files\\token.json";
+            var credentialsFile = "C:\\Users\\vanil\\source\\repos\\WebApplication1\\WebApplication1\\Files\\credentials.json";
+            var credentials = JObject.Parse(System.IO.File.ReadAllText(credentialsFile));
+            var tokens = JObject.Parse(System.IO.File.ReadAllText(tokenFile));
+
+            RestClient restClient = new RestClient();
+            RestRequest request = new RestRequest();
+
+            request.AddQueryParameter("client_id", credentials["client_id"].ToString());
+            request.AddQueryParameter("client_secret", credentials["client_secret"].ToString());
+            request.AddQueryParameter("grant_type", "refresh_token");
+            request.AddQueryParameter("refresh_token", tokens["refresh_token"].ToString());
+
+            restClient.BaseUrl = new System.Uri("https://oauth2.googleapis.com/token");
+            var response = restClient.Post(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                JObject newTokens = JObject.Parse(response.Content);
+                newTokens["refresh_token"] = tokens["refresh_token"].ToString();
+                System.IO.File.WriteAllText(tokenFile, newTokens.ToString());
+                return RedirectToAction("Index", "Home", new { status = "success" });
+            }
+
+            return View("Error");
+        }
+
+        public ActionResult RevokeToken()
+        {
+            var tokenFile = "C:\\Users\\vanil\\source\\repos\\WebApplication1\\WebApplication1\\Files\\token.json";
+            var credentialsFile = "C:\\Users\\vanil\\source\\repos\\WebApplication1\\WebApplication1\\Files\\credentials.json";
+            var tokens = JObject.Parse(System.IO.File.ReadAllText(tokenFile));
+
+            RestClient restClient = new RestClient();
+            RestRequest request = new RestRequest();
+
+            request.AddQueryParameter("token", tokens["access_token"].ToString());
+
+            restClient.BaseUrl = new System.Uri("https://oauth2.googleapis.com/revoke");
+            var response = restClient.Post(request);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                return RedirectToAction("Index", "Home", new { status = "success" });
+            }
+
+            return View("Error");
+
+        }
+
+        private ActionResult GetTokens(string code)
+        {
+            var tokenFile = "C:\\Users\\vanil\\source\\repos\\WebApplication1\\WebApplication1\\Files\\token.json";
+            var credentialsFile = "C:\\Users\\vanil\\source\\repos\\WebApplication1\\WebApplication1\\Files\\credentials.json";
+            var credentials = JObject.Parse(System.IO.File.ReadAllText(credentialsFile));
+
+            RestClient restClient = new RestClient();
+            RestRequest request = new RestRequest();
+
+            request.AddQueryParameter("client_id", credentials["client_id"].ToString());
+            request.AddQueryParameter("client_secret", credentials["client_secret"].ToString());
+            request.AddQueryParameter("code", code);
+            request.AddQueryParameter("grant_type", "authorization_code");
+            request.AddQueryParameter("redirect_uri", "https://localhost:44384/oauth/callback");
+
+            restClient.BaseUrl = new System.Uri("https://oauth2.googleapis.com/token");
+            var response = restClient.Post(request);
+
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                System.IO.File.WriteAllText(tokenFile, response.Content);
+                return RedirectToAction("Index", "Home");
+
+            }
+
+
+            return View("Error");
+
+
+
+        }
+    }
+}
