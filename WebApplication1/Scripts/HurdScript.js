92cc56393bdee12a96a71c0cec4df4700610366a
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2021-04-16 16:23:03 -0700

    Front End
    Javascript and images are being added to spice up the site.

---
+++ WebApplication1/Scripts/HurdScript.js
@@ -0,0 +1,46 @@
+﻿var direction = "right";
+var winWidth = window.innerWidth
+    || document.documentElement.clientWidth
+    || document.body.clientWidth;
+
+function MoveHeaderLeft() {
+    var x = -50;
+    var jumboPic = document.getElementById("JumboPic");
+ 
+    var movingImage = setInterval(() => {
+        if (winWidth < 600) {
+            if (x > 0 && x < 13) {
+                direction = "right";
+            }
+            if (x < -1590) {
+                direction = "left";
+            }
+
+
+            if (direction === "right") {
+                x = x - 0.4;
+                jumboPic.style.left = x + "px";
+            } else {
+                x = x + 0.4;
+                jumboPic.style.left = x + "px";
+            }
+        } else {
+            if (x > 0 && x < 13) {
+                direction = "right";
+            }
+            if (x < -690) {
+                direction = "left";
+            }
+
+
+            if (direction === "right") {
+                x = x - 0.4;
+                jumboPic.style.left = x + "px";
+            } else {
+                x = x + 0.4;
+                jumboPic.style.left = x + "px";
+            }}
+     
+    }, 10);
+
+}
