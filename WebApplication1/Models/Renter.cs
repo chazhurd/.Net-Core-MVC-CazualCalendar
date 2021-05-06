b7b3bfa4f1b8ac0290d837af9210566bdd9f1b00
Chaz Hurd <52184026+chazhurd@users.noreply.github.com>
2021-05-05 17:02:58 -0700

    Database
    Database for renter and equipment added along with a view.

---
+++ WebApplication1/Models/Renter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Renter
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Item { get; set; }
+
+    }
+}
